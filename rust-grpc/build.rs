8ce8761d3be86de1c9741fa7f77f9479b4b1c26e
hunshenshi <289517357@qq.com>
2023-11-17 18:22:20 +0800

    feat(code): refactor project

--- rust-grpc/build.rs
+++ rust-grpc/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   let proto_file = "./proto/vm_runtime.proto";
+   let proto_file = "../proto/vm_runtime.proto";
    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
    tonic_build::configure()
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
            .build_server(true)
            .file_descriptor_set_path(out_dir.join("runtime_descriptor.bin"))
            .out_dir("./src/grpc")
-           .compile(&[proto_file], &["proto"])?;
+           .compile(&[proto_file], &["../proto"])?;
 
    Ok(())
 }
